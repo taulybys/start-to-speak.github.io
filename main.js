c5041a19455a36d0cd4153659f79a62f4b5c9afd
Crsfil <61619347+Crsfil@users.noreply.github.com>
2020-05-18 21:41:03 +0300

    Хедер, футер белые иконки, отзывы с реальным текстом, счетчик уменьшенной ширины

--- main.js
+++
@@ -1,7 +0,0 @@
-var size = 120,
-    newsContent= $('.review-item-1-text'),
-    newsText = newsContent.text();
-    
-if(newsText.length > size){
-	newsContent.text(newsText.slice(0, size) + ' ...');
-}
